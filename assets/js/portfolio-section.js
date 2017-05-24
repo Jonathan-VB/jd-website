6b4b7e5149d30a40ac02bfa4e26e113e3fe06280
Jonathan-VB <jonathan@viralbamboo.com>
2017-05-24 22:38:14 +0100

    Hover Overlay for Portfolio Items
    - Added the overlay transition on hover and added dummy content overlay
    for each portfolio item.

--- assets/js/portfolio-section.js
+++ assets/js/portfolio-section.js
@@ -12,7 +12,12 @@ $(document).ready(function () {
     var width = getRandomSize(200, 400);
     var height =  getRandomSize(200, 400);
     // Gets random images of cats with the random width and height
+    // Ignore the messy layout, this JS will be removed once actual content is added.
+    allImages += '<div class="portfolio-grid-item__container">';
+    allImages += '<div class="portfolio-grid-item">';
     allImages += '<img src="https://placekitten.com/'+width+'/'+height+'" alt="pretty kitty">';
+    allImages += '<div class="portfolio-grid-item__overlay"><h2>Hello!!!</h2></div>';
+    allImages += '</div></div>';
   }
 
   $('.portfolio-grid').append(allImages);
