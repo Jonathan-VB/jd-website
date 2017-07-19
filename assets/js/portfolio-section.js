1f41aaaa495df41131ab8d97d155ec21a1070a73
Jonathan-VB <jonathanedempsey@gmail.com>
2017-07-19 16:11:23 +0100

    Portfolio Tile Overlay
    - Changed the portfolio tile overlay to always show the title and
    subtitle.
    - Removed the button as entire tile is within an anchor tag now.

--- assets/js/portfolio-section.js
+++ assets/js/portfolio-section.js
@@ -14,10 +14,11 @@ $(document).ready(function () {
     // Gets random images of cats with the random width and height
     // Ignore the messy layout, this JS will be removed once actual content is added.
     allImages += '<div class="portfolio-grid-item__container">';
-    allImages += '<div class="portfolio-grid-item">';
+    allImages += '<div class="portfolio-grid-item"><a href="#">';
     allImages += '<img src="https://placekitten.com/'+width+'/'+height+'" alt="pretty kitty">';
-    allImages += '<div class="portfolio-grid-item__overlay"><h2 class="portfolio-grid-item__overlay__title">Hello!!!</h2><button class="button">View Project</button></div>';
-    allImages += '</div></div>';
+    allImages += '<div class="portfolio-grid-item__overlay">';
+    allImages += '<h1 class="portfolio-grid-item__overlay__title">PROJECT TITLE</h1><h2 class="portfolio-grid-item__overlay__subtitle">Project Subtitle</h2>';
+    allImages += '</div></div></a></div>';
   }
 
   $('.portfolio-grid').append(allImages);
