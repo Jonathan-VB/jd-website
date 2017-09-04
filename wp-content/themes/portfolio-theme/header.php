fccb45f9c7e453f4d284f9dd64ccd6b07e9311d7
Jonathan-VB <jonathanedempsey@gmail.com>
2017-09-04 22:17:46 +0100

    Home URL
    - Forgot home url, oops

--- wp-content/themes/portfolio-theme/header.php
+++ wp-content/themes/portfolio-theme/header.php
@@ -26,7 +26,7 @@
 
     <?php wp_nav_menu( array( 'menu' => 'main-left' ) ); ?>
 
-      <a class="nav-logo" href="/"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" class="logo"/></a>
+      <a class="nav-logo" href="<?php echo get_site_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" class="logo"/></a>
 
     <?php wp_nav_menu( array( 'menu' => 'main-right' ) ); ?>
 
