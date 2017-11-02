1012be982c701c565ba6c1c5d8981c36edd277e7
Jonathan-VB <jonathanedempsey@gmail.com>
2017-11-02 22:05:26 +0000

    New Portfolio Grid Markup/Styles
    - Suuuuuper greasy mark up and stylings. Clean up needed.

--- wp-content/themes/portfolio-theme/template-portfolio.php
+++ wp-content/themes/portfolio-theme/template-portfolio.php
@@ -10,7 +10,32 @@ get_header(); ?>
 
 
 <div class="container">
-
+  <section class="portfolio-grid">
+    <div class="grid">
+      <figure class="effect-winston">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/block-census-ireland.jpg" alt=""/>
+        <figcaption>
+          <h2>Census <span>Ireland</span></h2>
+          <p>
+            <a href="#"><i class="fa fa-fw fa-star-o">Case Study</i></a>
+            <a href="#"><i class="fa fa-fw fa-comments-o"></i></a>
+            <a href="#"><i class="fa fa-fw fa-envelope-o"></i></a>
+          </p>
+        </figcaption>
+      </figure>
+      <figure class="effect-winston">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/block-locomotive.jpg" alt=""/>
+        <figcaption>
+          <h2>Loco<span>motive</span></h2>
+          <p>
+            <a href="#"><i class="fa fa-fw fa-star-o">Case Study</i></a>
+            <a href="#"><i class="fa fa-fw fa-comments-o"></i></a>
+            <a href="#"><i class="fa fa-fw fa-envelope-o"></i></a>
+          </p>
+        </figcaption>
+      </figure>
+    </div>
+  </section>
 </div>
 
 
