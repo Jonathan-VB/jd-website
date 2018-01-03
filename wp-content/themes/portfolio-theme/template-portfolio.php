eab1e8db2aa810d92867fc8f5dc97c734f10501d
Jonathan-VB <jonathanedempsey@gmail.com>
2018-01-03 21:43:49 +0000

    Markup Cleanup
    - Cleaning up the markup for the portfolio page panels

--- wp-content/themes/portfolio-theme/template-portfolio.php
+++ wp-content/themes/portfolio-theme/template-portfolio.php
@@ -11,30 +11,29 @@ get_header(); ?>
 
 <div class="container">
   <section class="portfolio-grid">
-    <div class="grid">
-      <figure class="effect-winston">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/block-census-ireland.jpg" alt=""/>
-        <figcaption>
-          <h2>Census <span>Ireland</span></h2>
-          <p>
-            <a href="#"><i class="fa fa-fw fa-star-o">Case Study</i></a>
-            <a href="#"><i class="fa fa-fw fa-comments-o"></i></a>
-            <a href="#"><i class="fa fa-fw fa-envelope-o"></i></a>
-          </p>
-        </figcaption>
-      </figure>
-      <figure class="effect-winston">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/block-locomotive.jpg" alt=""/>
-        <figcaption>
-          <h2>Loco<span>motive</span></h2>
-          <p>
-            <a href="#"><i class="fa fa-fw fa-star-o">Case Study</i></a>
-            <a href="#"><i class="fa fa-fw fa-comments-o"></i></a>
-            <a href="#"><i class="fa fa-fw fa-envelope-o"></i></a>
-          </p>
-        </figcaption>
-      </figure>
-    </div>
+    <figure class="portfolio-grid__item effect-winston">
+      <img class="portfolio-grid__item__image" src="<?php echo get_template_directory_uri(); ?>/assets/images/block-census-ireland.jpg" alt=""/>
+      <figcaption class="portfolio-grid__item__content">
+        <h2 class="portfolio-grid__item__content__header">Census <span>Ireland</span></h2>
+        <p class="portfolio-grid__item__content__paragraph">
+          <a class="portfolio-grid__item__content__link" href="#"><i class="fa fa-fw fa-star-o">Case Study</i></a>
+          <a class="portfolio-grid__item__content__link" href="#"><i class="fa fa-fw fa-comments-o"></i></a>
+          <a class="portfolio-grid__item__content__link" href="#"><i class="fa fa-fw fa-envelope-o"></i></a>
+        </p>
+      </figcaption>
+    </figure>
+
+    <figure class="portfolio-grid__item effect-winston">
+      <img class="portfolio-grid__item__image" src="<?php echo get_template_directory_uri(); ?>/assets/images/block-locomotive.jpg" alt=""/>
+      <figcaption class="portfolio-grid__item__content">
+        <h2 class="portfolio-grid__item__content__header">Loco<span>motive</span></h2>
+        <p class="portfolio-grid__item__content__paragraph">
+          <a class="portfolio-grid__item__content__link" href="#"><i class="fa fa-fw fa-star-o">Case Study</i></a>
+          <a class="portfolio-grid__item__content__link" href="#"><i class="fa fa-fw fa-comments-o"></i></a>
+          <a class="portfolio-grid__item__content__link" href="#"><i class="fa fa-fw fa-envelope-o"></i></a>
+        </p>
+      </figcaption>
+    </figure>
   </section>
 </div>
 
