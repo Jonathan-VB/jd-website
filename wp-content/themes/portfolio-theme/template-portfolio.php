3b1b5621efc5fdd02f2952639f45ce011d2f094b
Jonathan-VB <jonathanedempsey@gmail.com>
2018-01-06 14:25:00 +0000

    Portfolio Panel Styling
    - Hover stylings.
    - Added subheader fade in when panel is hovered over.

--- wp-content/themes/portfolio-theme/template-portfolio.php
+++ wp-content/themes/portfolio-theme/template-portfolio.php
@@ -14,11 +14,10 @@ get_header(); ?>
     <figure class="portfolio-grid__item">
       <img class="portfolio-grid__item__image" src="<?php echo get_template_directory_uri(); ?>/assets/images/block-census-ireland.jpg" alt=""/>
       <figcaption class="portfolio-grid__item__content">
-        <h2 class="portfolio-grid__item__content__header">Census <span>Ireland</span></h2>
+        <h2 class="portfolio-grid__item__content__header">Census Ireland</h2>
+        <h3 class="portfolio-grid__item__content__subheader">Data Visualisation using Tableau</h3>
         <p class="portfolio-grid__item__content__paragraph">
           <a class="portfolio-grid__item__content__link" href="#"><i class="fa fa-fw fa-star-o">Case Study</i></a>
-          <a class="portfolio-grid__item__content__link" href="#"><i class="fa fa-fw fa-comments-o"></i></a>
-          <a class="portfolio-grid__item__content__link" href="#"><i class="fa fa-fw fa-envelope-o"></i></a>
         </p>
       </figcaption>
     </figure>
@@ -26,11 +25,10 @@ get_header(); ?>
     <figure class="portfolio-grid__item">
       <img class="portfolio-grid__item__image" src="<?php echo get_template_directory_uri(); ?>/assets/images/block-locomotive.jpg" alt=""/>
       <figcaption class="portfolio-grid__item__content">
-        <h2 class="portfolio-grid__item__content__header">Loco<span>motive</span></h2>
+        <h2 class="portfolio-grid__item__content__header">Locomotive</h2>
+        <h3 class="portfolio-grid__item__content__subheader">Data Visualisation using Tableau</h3>
         <p class="portfolio-grid__item__content__paragraph">
           <a class="portfolio-grid__item__content__link" href="#"><i class="fa fa-fw fa-star-o">Case Study</i></a>
-          <a class="portfolio-grid__item__content__link" href="#"><i class="fa fa-fw fa-comments-o"></i></a>
-          <a class="portfolio-grid__item__content__link" href="#"><i class="fa fa-fw fa-envelope-o"></i></a>
         </p>
       </figcaption>
     </figure>
