8d8ba9c2c3804c268fc2973d9d9050d548c8ce14
Jonathan-VB <jonathanedempsey@gmail.com>
2018-01-06 14:57:13 +0000

    - Portfolio Panel Clickable
    - Entire portfolio panel is now clickable instead of just the 'Case
    Study' text.

--- wp-content/themes/portfolio-theme/template-portfolio.php
+++ wp-content/themes/portfolio-theme/template-portfolio.php
@@ -11,27 +11,27 @@ get_header(); ?>
 
 <div class="container">
   <section class="portfolio-grid">
-    <figure class="portfolio-grid__item">
+    <a class="portfolio-grid__item" href="#">
       <img class="portfolio-grid__item__image" src="<?php echo get_template_directory_uri(); ?>/assets/images/block-census-ireland.jpg" alt=""/>
       <figcaption class="portfolio-grid__item__content">
         <h2 class="portfolio-grid__item__content__header">Census Ireland</h2>
         <h3 class="portfolio-grid__item__content__subheader">Data Visualisation using Tableau</h3>
         <p class="portfolio-grid__item__content__paragraph">
-          <a class="portfolio-grid__item__content__link" href="#"><i class="fa fa-fw fa-star-o">Case Study</i></a>
+          <span class="portfolio-grid__item__content__link" href="#"><i class="fa fa-fw fa-star-o">Case Study</i></span>
         </p>
       </figcaption>
-    </figure>
+    </a>
 
-    <figure class="portfolio-grid__item">
+    <a class="portfolio-grid__item" href="#">
       <img class="portfolio-grid__item__image" src="<?php echo get_template_directory_uri(); ?>/assets/images/block-locomotive.jpg" alt=""/>
       <figcaption class="portfolio-grid__item__content">
         <h2 class="portfolio-grid__item__content__header">Locomotive</h2>
         <h3 class="portfolio-grid__item__content__subheader">Data Visualisation using Tableau</h3>
         <p class="portfolio-grid__item__content__paragraph">
-          <a class="portfolio-grid__item__content__link" href="#"><i class="fa fa-fw fa-star-o">Case Study</i></a>
+          <span class="portfolio-grid__item__content__link"><i class="fa fa-fw fa-star-o">Case Study</i></span>
         </p>
       </figcaption>
-    </figure>
+    </a>
   </section>
 </div>
 
