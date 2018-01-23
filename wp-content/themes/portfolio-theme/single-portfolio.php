4a26740f58bef3f590a47f83b2f3f836177b6564
Jonathan-VB <jonathanedempsey@gmail.com>
2018-01-23 21:28:12 +0000

    Portfolio Excerpt
    - Added the excerpt to the single portfolio page.

--- wp-content/themes/portfolio-theme/single-portfolio.php
+++ wp-content/themes/portfolio-theme/single-portfolio.php
@@ -36,7 +36,7 @@ if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 <section class="intro">
   <div class="container container__intro">
-    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.</p>
+    <p><?php echo the_field( 'portfolio_excerpt' ); ?></p>
   </div>
 
   <span class="divider__circle--white"></span>
