5e706e25cb4158c2f5533a17c4120af25dd669d0
Jonathan-VB <jonathanedempsey@gmail.com>
2018-03-27 18:23:52 +0100

    Next Previous Mobile Styles
    - Added mobile styling for the Next and Previous links.

--- wp-content/themes/portfolio-theme/single-portfolio.php
+++ wp-content/themes/portfolio-theme/single-portfolio.php
@@ -163,13 +163,17 @@ if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 <section class="previous-next">
   <a href="<?php echo $prevLink; ?>" class="previous" style="background: linear-gradient(rgba(0,0,0, .5), rgba(0,0,0, .5)), url(<?php echo $prevImage[0]; ?>) no-repeat center center; background-size: cover;">
-    <span>Previous:</span>
-    <p><?php echo $prevTitle; ?></p>
+    <div class="previous__content">
+      <span>Previous:</span>
+      <p><?php echo $prevTitle; ?></p>
+    </div>
   </a>
 
   <a href="<?php echo $nextLink ?>" class="next" style="background: linear-gradient(rgba(0,0,0, .5), rgba(0,0,0, .5)), url(<?php echo $nextImage[0]; ?>) no-repeat center center; background-size: cover;">
-    <span>Next:</span>
-    <p><?php echo $nextTitle; ?></p>
+    <div class="next__content">
+      <span>Next:</span>
+      <p><?php echo $nextTitle; ?></p>
+    </div>
   </a>
 </section>
 
