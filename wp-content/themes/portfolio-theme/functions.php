370cbb0fec7c55aebbc0749a8375bcfaa427c0cb
Jonathan-VB <jonathanedempsey@gmail.com>
2018-03-31 23:23:23 +0100

    Moving Folders
    - Moved the fonts and images folder into the assets-prod folder.

--- wp-content/themes/portfolio-theme/functions.php
+++ wp-content/themes/portfolio-theme/functions.php
@@ -11,7 +11,7 @@ add_theme_support( 'post-thumbnails' );
  * Enqueue styles.
  */
 function jd_theme_styles() {
-  wp_enqueue_style( 'main_css', get_template_directory_uri() . '/assets/css/main.css' );
+  wp_enqueue_style( 'main_css', get_template_directory_uri() . '/assets-prod/css/main.css' );
 }
 add_action( 'wp_enqueue_scripts', 'jd_theme_styles' );
 
@@ -21,10 +21,10 @@ add_action( 'wp_enqueue_scripts', 'jd_theme_styles' );
  */
 function jd_theme_js() {
   if( is_front_page() ) {
-    wp_enqueue_script( 'home_js', get_template_directory_uri() . '/assets/js/home.js', array('jquery'), '', true );
+    wp_enqueue_script( 'home_js', get_template_directory_uri() . '/assets-prod/js/home.js', array('jquery'), '', true );
   }
 
-  wp_enqueue_script( 'main_js', get_template_directory_uri() . '/assets/js/app.js', array('jquery'), '', true );
+  wp_enqueue_script( 'main_js', get_template_directory_uri() . '/assets-prod/js/app.js', array('jquery'), '', true );
 }
 add_action( 'wp_enqueue_scripts', 'jd_theme_js' );
 
