b944a4fc8ad1a32e052d2093577b350acf28fc34
Jonathan-VB <jonathanedempsey@gmail.com>
2018-04-05 19:08:23 +0100

    Revert "Wordfence Update"
    This reverts commit be55e23e67477e46af6791708d3456e24de54268.

--- wp-content/themes/portfolio-theme/functions.php
+++ wp-content/themes/portfolio-theme/functions.php
@@ -70,27 +70,3 @@ function create_posttype_portfolio() {
   );
 }
 add_action( 'init', 'create_posttype_portfolio' );
-
-
-/**
- * get latest post
- * use in loop if ( is_latest() ) { stuff; }
- **/
-function is_latest() {
-  global $post;
-  $loop = get_posts( 'numberposts=1' );
-  $latest = $loop[0]->ID;
-  return ( $post->ID == $latest ) ? true : false;
-}
-
-
-/**
- * get first post
- * use in loop if ( is_first() ) { stuff; }
- **/
-function is_first() {
-  global $post;
-  $loop = get_posts( 'numberposts=1&order=ASC' );
-  $first = $loop[0]->ID;
-  return ( $post->ID == $first ) ? true : false;
-}
