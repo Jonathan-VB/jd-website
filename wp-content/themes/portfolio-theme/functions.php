3ae3532353816be254845f964e3754c98b5e8685
Jonathan-VB <jonathanedempsey@gmail.com>
2018-05-17 22:04:43 +0100

    Contact Form 7 Styles
    - Added some stylings for Contact Form 7 form.

--- wp-content/themes/portfolio-theme/functions.php
+++ wp-content/themes/portfolio-theme/functions.php
@@ -11,7 +11,7 @@ add_theme_support( 'post-thumbnails' );
  * Enqueue styles.
  */
 function jd_theme_styles() {
-  wp_enqueue_style( 'main_css', get_template_directory_uri() . '/assets-prod/css/main.css' );
+  wp_enqueue_style( 'main_css', get_template_directory_uri() . '/assets/css/main.css' );
 }
 add_action( 'wp_enqueue_scripts', 'jd_theme_styles' );
 
